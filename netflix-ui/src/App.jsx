2ad2ff0c9585f4e772a39cd365897f2fd810651d
Abed Nganga <ngangaabed@gmail.com>
2024-01-06 00:17:51 +0300

    Modified app jsx

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -1,12 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
+export default function App() {
   return (
-    <div className="">
-
-    </div>
-  );
+    <div>App</div>
+  )
 }
-
-export default App;
