8a9c18ff4b24b04b5ce216e31147e336c048d5c5
Abed Nganga <ngangaabed@gmail.com>
2024-01-07 01:31:10 +0300

    Modified signup structure

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -3,10 +3,11 @@ import styled from "styled-components"
 import BackgroundImage from "../components/BackgroundImage "
 import Header from "../components/Header"
 
+const Container = styled.div
 
 export default function Signup() {
   return (
-    <Container>
+    <container>
       <BackgroundImage />
       <Header />
       <div className='body flex column a-center j-center'>
@@ -15,10 +16,13 @@ export default function Signup() {
           <h4>Watch anywhere, cancel anytime.</h4>
           <h6>Ready to watch? Enter your email to create or start membership</h6>
         </div>
-        <div>
-          
+        <div className='form'>
+          <input type='email' name='email' className='' placeholder='Email Address'/>
+          <input type='password' placeholder='Password' name='password'/>
+          <button>Get Started</button>
         </div>
+        <button>Log In</button>
       </div>
-    </Container>
+    </container>
   )
 }
