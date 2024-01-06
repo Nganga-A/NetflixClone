10e6d1992e96394f449e47c765a6bfd8f497e8ad
Abed Nganga <ngangaabed@gmail.com>
2024-01-07 01:16:00 +0300

    Initial SignUp structure

--- netflix-ui/src/pages/Signup.jsx
+++ netflix-ui/src/pages/Signup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import styled from "styled-components"
+import BackgroundImage from "../components/BackgroundImage "
+import Header from "../components/Header"
+
 
 export default function Signup() {
   return (
-    <container>Signup</div>
+    <Container>
+      <BackgroundImage />
+      <Header />
+      <div className='body flex column a-center j-center'>
+        <div className='text flex column'>
+          <h1>Unlimited movies, TV shows and more</h1>
+          <h4>Watch anywhere, cancel anytime.</h4>
+          <h6>Ready to watch? Enter your email to create or start membership</h6>
+        </div>
+        <div>
+          
+        </div>
+      </div>
+    </Container>
   )
 }
