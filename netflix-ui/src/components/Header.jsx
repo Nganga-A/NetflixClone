04d696d6d0bc133fb85bbbe5a501aa394a73ae36
Abed Nganga <ngangaabed@gmail.com>
2024-01-07 15:04:27 +0300

    Structured header

--- netflix-ui/src/components/Header.jsx
+++ netflix-ui/src/components/Header.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import styled from "styled-components"
+import logo from "../assets/logo.png"
 
-export default function Header() {
+export default function Header(props) {
   return (
-    <div>Header</div>
+    <Container>
+      <div className="logo">
+        <img src={logo} alt="logo" />
+      </div>
+      <button onClick={() => navigate(props.login ? "/login" : "/signup")}> {props.login ? "Log in" : "Sign in"} </button>
+    </Container>
   )
 }
+
+const Container = styled.div``;
