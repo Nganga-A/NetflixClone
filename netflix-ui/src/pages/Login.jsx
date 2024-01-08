3787ba8de5963108155f1abae834c81a3d665871
Abed Nganga <ngangaabed@gmail.com>
2024-01-08 23:35:19 +0300

    Structured Login form layout

--- netflix-ui/src/pages/Login.jsx
+++ netflix-ui/src/pages/Login.jsx
@@ -1,7 +1,66 @@
-import React from 'react'
+import React, { useState } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import { firebaseAuth } from "../utils/firebase-config.js";
+import BackgroundImage from "../components/BackgroundImage "
+import Header from "../components/Header";
 
-export default function Login() {
+// Login component
+function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  // Handle login attempt
+  const handleLogin = async () => {
+    try {
+      await signInWithEmailAndPassword(firebaseAuth, email, password);
+    } catch (error) {
+      console.log(error.code);
+      
+    }
+  };
+
+  // Check if the user is already authenticated, then redirect
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) navigate("/");
+  });
+
+
   return (
-    <div>Login</div>
-  )
+    <Container>
+      <BackgroundImage />
+      <div className="content">
+        <Header />
+        <div className="form-container flex column a-center j-center">
+          <div className="form flex column a-center j-center">
+            <div className="title">
+              <h3>Login</h3>
+            </div>
+            <div className="container flex column">
+              <input
+                type="text"
+                placeholder="Email"
+                onChange={(e) => setEmail(e.target.value)}
+                value={email}
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+              />
+              <button onClick={handleLogin}>Login to your account</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+`;
+
+export default Login;
