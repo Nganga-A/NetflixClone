c28cc697d3382313b6988b657eb17a78a747000e
Abed Nganga <ngangaabed@gmail.com>
2024-01-10 02:10:25 +0300

    Added search, signout to navbar

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,18 +1,24 @@
-import React from 'react'
+import React, {useState} from 'react'
 import styled from "styled-components"
 import logo from "../assets/logo.png"
 import {Link} from "react-router-dom"
-
+import { firebaseAuth } from "../utils/firebase-config.js";
+import { FaPowerOff, FaSearch } from "react-icons/fa";
+import { signOut } from "firebase/auth";
 
 export default function Navbar({isScrolled}) {
+
+    const [showSearch, setShowSearch] = useState(false);
+    const [inputHover, setInputHover] = useState(false);
 
     const links = [
         { name: "Home", link: "/" },
         { name: "TV Shows", link: "/tv" },
         { name: "Movies", link: "/movies" },
         { name: "My List", link: "/mylist" },
-      ];
+    ];
 
+    
   return (
     <Container>
         <nav className={`${isScrolled ? "scrolled" : ""} flex`}>
@@ -33,9 +39,31 @@ export default function Navbar({isScrolled}) {
                 </ul>
             </div>
             <div className="right flex a-center">
-                <div className={`search`}>
-
+                <div className={`search ${showSearch ? "show-search" : ""}`}>
+                    <button 
+                        onFocus={() => setShowSearch(true)}
+                        onBlur={() => {
+                        if (!inputHover) {
+                        setShowSearch(false);
+                            }
+                        }}
+                    >
+                        <FaSearch />
+                    </button>
+                    <input
+                        type="text"
+                        placeholder="Search"
+                        onMouseEnter={() => setInputHover(true)}
+                        onMouseLeave={() => setInputHover(false)}
+                        onBlur={() => {
+                            setShowSearch(false);
+                            setInputHover(false);
+                        }}
+                    />
                 </div>
+                <button onClick={() => signOut(firebaseAuth)}>
+                    <FaPowerOff />
+                </button>
             </div>
         </nav>
     </Container>
