79b782aec7b61e47befdb725312ee6c0240174ae
Abed Nganga <ngangaabed@gmail.com>
2024-01-10 01:10:25 +0300

    Added event handler for window scroll

--- netflix-ui/src/pages/Netflix.jsx
+++ netflix-ui/src/pages/Netflix.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../components/Navbar';
 
 export default function Netflix() {
+
+  useState [isScrolled, setIsScrolled] = useState(false);
+
+ // Event handler for window scroll
+ window.onscroll = () => {
+  // Update isScrolled state based on the Y offset of the page
+  setIsScrolled(window.pageYOffset === 0 ? false : true);
+
+  // Cleanup: Remove the scroll event listener when the component is unmounted
+  return () => (window.onscroll = null);
+};
+
   return (
-    <div>Netflix</div>
+    <div>
+      <Navbar isSCrolled={isScrolled} />
+    </div>
   )
 }
 
