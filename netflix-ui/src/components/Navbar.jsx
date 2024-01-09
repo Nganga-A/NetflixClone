035eafe6d30b80e962754e22a2cc35d24496167b
Abed Nganga <ngangaabed@gmail.com>
2024-01-10 01:32:39 +0300

    Added Navbar links

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,9 +1,46 @@
 import React from 'react'
+import styled from "styled-components"
+import logo from "../assets/logo.png"
+import {Link} from "react-router-dom"
 
-function Navbar() {
+
+export default function Navbar({isScrolled}) {
+
+    const links = [
+        { name: "Home", link: "/" },
+        { name: "TV Shows", link: "/tv" },
+        { name: "Movies", link: "/movies" },
+        { name: "My List", link: "/mylist" },
+      ];
+
   return (
-    <div>Navbar</div>
-  )
+    <Container>
+        <nav className={`${isScrolled ? "scrolled" : ""} flex`}>
+            <div className='left flex a-center'>
+                <div className='brand flex a-center j-center'>
+                    <img src={logo} alt="logo" />
+                </div>
+                <ul className='link flex'>
+                    {
+                        links.map(({pname,link}) => {
+                            return (
+                                <li key={pname}>
+                                    <Link to={link}>{pname}</Link>
+                              </li>
+                            );
+                        })
+                    }
+                </ul>
+            </div>
+            <div className="right flex a-center">
+                <div className={`search`}>
+
+                </div>
+            </div>
+        </nav>
+    </Container>
+  );
 }
 
-export default Navbar
+
+const Container = styled.div``;
