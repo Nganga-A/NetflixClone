199624fc7386a1e63c51f87f64ed1de2621df56a
Abed Nganga <ngangaabed@gmail.com>
2024-01-10 03:03:02 +0300

    Styled navbar

--- netflix-ui/src/components/Navbar.jsx
+++ netflix-ui/src/components/Navbar.jsx
@@ -1,10 +1,10 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from "styled-components"
 import logo from "../assets/logo.png"
-import {Link} from "react-router-dom"
+import {Link, useNavigate} from "react-router-dom"
 import { firebaseAuth } from "../utils/firebase-config.js";
 import { FaPowerOff, FaSearch } from "react-icons/fa";
-import { signOut } from "firebase/auth";
+import { onAuthStateChanged, signOut } from "firebase/auth";
 
 export default function Navbar({isScrolled}) {
 
@@ -18,21 +18,34 @@ export default function Navbar({isScrolled}) {
         { name: "My List", link: "/mylist" },
     ];
 
+    const navigate = useNavigate();
+
+    //logging out redirect
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(firebaseAuth, (currentUser) => {
+        if (!currentUser) {
+            navigate("/login");
+        }
+        });
+
+        // Cleanup function
+        return () => unsubscribe();
+        }, []);
     
-  return (
+    return (
     <Container>
         <nav className={`${isScrolled ? "scrolled" : ""} flex`}>
             <div className='left flex a-center'>
                 <div className='brand flex a-center j-center'>
                     <img src={logo} alt="logo" />
                 </div>
-                <ul className='link flex'>
+                <ul className='links flex'>
                     {
-                        links.map(({pname,link}) => {
+                        links.map(({name,link}) => {
                             return (
-                                <li key={pname}>
-                                    <Link to={link}>{pname}</Link>
-                              </li>
+                                <li key={name}>
+                                    <Link to={link}>{name}</Link>
+                                </li>
                             );
                         })
                     }
@@ -71,4 +84,95 @@ export default function Navbar({isScrolled}) {
 }
 
 
-const Container = styled.div``;
+const Container = styled.div`
+    .scrolled {
+        background-color: black;
+    }
+    nav {
+        position: sticky;
+        top: 0;
+        height: 6rem;
+        width: 100%;
+        justify-content: space-between;
+        position: fixed;
+        top: 0;
+        z-index: 2;
+        padding: 0 1rem;
+        align-items: center;
+        transition: 0.3s ease-in-out;
+    .left {
+        gap: 2rem;
+        .brand {
+            img {
+                height: 4rem;
+            }
+        }
+    .links {
+        list-style-type: none;
+        gap: 2rem;
+        li {
+            a {
+                color: white;
+                text-decoration: none;
+                }
+            }
+        }
+    }
+    .right {
+        gap: 1rem;
+        button {
+        background-color: transparent;
+        border: none;
+        cursor: pointer;
+        &:focus {
+        outline: none;
+        }
+        svg {
+            color: #f34242;
+            font-size: 1.2rem;
+            }
+    }
+    .search {
+        display: flex;
+        gap: 0.4rem;
+        align-items: center;
+        justify-content: center;
+        padding: 0.2rem;
+        padding-left: 0.5rem;
+        button {
+            background-color: transparent;
+            border: none;
+            &:focus {
+                outline: none;
+            }
+        svg {
+            color: white;
+            font-size: 1.2rem;
+        }
+        }
+        input {
+            width: 0;
+            opacity: 0;
+            visibility: hidden;
+            transition: 0.3s ease-in-out;
+            background-color: transparent;
+            border: none;
+            color: white;
+            &:focus {
+                outline: none;
+            }
+            }
+        }
+    .show-search {
+        border: 1px solid white;
+        background-color: rgba(0, 0, 0, 0.6);
+        input {
+            width: 100%;
+            opacity: 1;
+            visibility: visible;
+            padding: 0.3rem;
+        }
+    }
+    }
+}
+`;
