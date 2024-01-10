c0b6af8905c3cc2e90bc7c9aacf4424acd810073
Abed Nganga <ngangaabed@gmail.com>
2024-01-10 03:03:28 +0300

    Minor modifications on login

--- netflix-ui/src/pages/Login.jsx
+++ netflix-ui/src/pages/Login.jsx
@@ -49,6 +49,7 @@ function Login() {
               <input
                 type="text"
                 placeholder="Email"
+                name="email"
                 onChange={(e) => setEmail(e.target.value)}
                 value={email}
               />
