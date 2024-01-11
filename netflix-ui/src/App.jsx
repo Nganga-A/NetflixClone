a094e464d9aa89b99d935d550892160aae552bb1
Abed Nganga <ngangaabed@gmail.com>
2024-01-12 01:22:35 +0300

    Added player route

--- netflix-ui/src/App.jsx
+++ netflix-ui/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Netflix from './pages/Netflix';
+import Player from './pages/Player';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/" element={<Netflix />} />
+          <Route path="/player" element={<Player />} />
         </Routes>
       </div>
     </Router>
