f6e2e727802c5aebea90163c7008d397da0cdf8d
Abed Nganga <ngangaabed@gmail.com>
2024-01-13 11:43:48 +0300

    Made notAvailable feedback message dynamic

--- netflix-ui/src/components/NotAvailable.jsx
+++ netflix-ui/src/components/NotAvailable.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { useLocation } from "react-router-dom";
 
 export default function NotAvailable() {
+
+  const location = useLocation();
+  const isMoviesPage = location.pathname === "/movies";
+
   return (
-    <h2 className="not-available">
-      No Movies available for the selected genre. Please select a different
-      genre.
+    <h2 className="not-available"> 
+    {isMoviesPage ?
+      "No Movies available for the selected genre. Please select a different genre." 
+      : "No TV shows available for the selected genre. Please select a different genre."}
     </h2> 
   );
 }
