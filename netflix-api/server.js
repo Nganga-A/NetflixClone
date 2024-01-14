1607c6cdb48d9a0b683525fa610b4674b3f739b7
Abed Nganga <ngangaabed@gmail.com>
2024-01-15 02:01:14 +0300

    Stored url in constants

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -2,9 +2,9 @@ const express = require("express");
 const cors = require("cors");
 const userRoutes = require("./routes/UserRoutes");
 const mongoose = require("mongoose");
+import  url  from"./utils/constants";
 
 const app = express();
-const url = "mongodb+srv://test:123@cluster0.rppvb8f.mongodb.net/?retryWrites=true&w=majority"
 
 app.use(cors());
 app.use(express.json());
