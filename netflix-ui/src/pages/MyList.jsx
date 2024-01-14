063c4ac2e93a7cc2d8f2fa26f1faf6ac0970b6b2
Abed Nganga <ngangaabed@gmail.com>
2024-01-15 01:19:46 +0300

    Structured and styled mylist page

--- netflix-ui/src/pages/MyList.jsx
+++ netflix-ui/src/pages/MyList.jsx
@@ -1,7 +1,70 @@
-import React from 'react'
+import axios from "axios";
+import { onAuthStateChanged } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { firebaseAuth } from "../utils/firebase-config.js";
+import Card from "../components/Card";
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
+import { getUsersLikedMovies } from "../store";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function MyList() {
+  const movies = useSelector((state) => state.netflix.movies);
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [isScrolled, setIsScrolled] = useState(false);
+  const [email, setEmail] = useState(undefined);
+
+  onAuthStateChanged(firebaseAuth, (currentUser) => {
+    if (currentUser) setEmail(currentUser.email);
+    else navigate("/login");
+  });
+
+  useEffect(() => {
+    if (email) {
+      dispatch(getUsersLikedMovies(email));
+    }
+  }, [email]);
+
+  window.onscroll = () => {
+    setIsScrolled(window.pageYOffset === 0 ? false : true);
+    return () => (window.onscroll = null);
+  };
+
   return (
-    <div>MyList</div>
-  )
+    <Container>
+      <Navbar isScrolled={isScrolled} />
+      <div className="content flex column">
+        <h1>My List</h1>
+        <div className="grid flex">
+          {movies.map((movie, index) => {
+            return (
+              <Card
+                movieData={movie}
+                index={index}
+                key={movie.id}
+                isLiked={true}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  .content {
+    margin: 2rem;
+    margin-top: 8rem;
+    gap: 3rem;
+    h1 {
+      margin-left: 1rem;
+    }
+    .grid {
+      flex-wrap: wrap;
+      gap: 1rem;
+    }
+  }
+`;
