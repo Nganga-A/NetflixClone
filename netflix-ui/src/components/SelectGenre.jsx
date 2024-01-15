5a42f0e748e8271e7afa74242f314feea5e2d7eb
Abed Nganga <ngangaabed@gmail.com>
2024-01-15 03:59:08 +0300

    A bit of styling

--- netflix-ui/src/components/SelectGenre.jsx
+++ netflix-ui/src/components/SelectGenre.jsx
@@ -1,59 +1,65 @@
 import React from "react";
 import { useLocation } from "react-router-dom";
-import { useDispatch} from "react-redux";
+import { useDispatch } from "react-redux";
 import styled from "styled-components";
 import { fetchDataByGenre } from "../store";
 
-
 export default function SelectGenre({ genres, type }) {
+  const dispatch = useDispatch();
+  const location = useLocation();
+  const isMoviesPage = location.pathname === "/movies";
 
-    const dispatch = useDispatch();
-    const location = useLocation();
-    const isMoviesPage = location.pathname === "/movies";
-
-    // Render a dropdown select element for genres
-    return (
-        <Container className="flex">
-        <span>{isMoviesPage ? "Movies" : "Tv Shows"} </span>
-        <Select
-            className="flex"
-            onChange={(e) => {
-                // Dispatch an action to fetch data based on the selected genre
-                dispatch(
-                    fetchDataByGenre({
-                        genres,
-                        genre: e.target.value,
-                        type,
-                    })
-                );
-            }}
-        >
-            {/* Map through genres and render an option for each */}
-            {genres.map((genre) => {
-                return (
-                    <option value={genre.id} key={genre.id}>
-                        {genre.name}
-                    </option>
-                );
-            })}
-        </Select>
-        </Container>
-    );
+  return (
+    <Container className="flex">
+      <span>{isMoviesPage ? "Movies" : "Tv Shows"} </span>
+      <CustomSelect
+        onChange={(e) => {
+          dispatch(
+            fetchDataByGenre({
+              genres,
+              genre: e.target.value,
+              type,
+            })
+          );
+        }}
+      >
+        {genres.map((genre) => (
+          <Option key={genre.id} value={genre.id}>
+            {genre.name}
+          </Option>
+        ))}
+      </CustomSelect>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    display: flex;
-    align-items: center;
-    span {
-        font-size:1.2rem;
-        margin-left:3rem;
-    }
+  display: flex;
+  align-items: center;
+  span {
+    font-size: 1.2rem;
+    margin-left: 3rem;
+  }
 `;
 
-const Select = styled.select`
-    margin-left: 1rem;
-    cursor: pointer;
-    font-size: 1.2rem;
-    background-color: rgba(0, 0, 0, 0.9);
-    color: white;
+const CustomSelect = styled.select`
+  cursor: pointer;
+  font-size: 1.2rem;
+  background-color: rgba(0, 0, 0, 1);
+  color: white;
+  padding: 0.1rem;
+  border-radius: 4px;
+  margin-left: 1rem;
+
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Option = styled.option`
+  padding: 1rem;
+  display: inline-block;
+  width: 100%;
+  box-sizing: border-box;
+  height: 5rem;
 `;
